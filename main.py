00a857bc0e52ce85a49f160ea2b08dedbc4107a5
dm1itri <dimma-18@yandex.ru>
2022-03-22 23:15:33 +0300

    Добавление БД 1.0

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from additional_functions import parse_news
+from data import db_session
+from data.users import User
+from forms.user import RegisterForm, LoginForm
+
+
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
 def index():
     return render_template('base.html')
+
+
+@app.route('/technical_class')
+def technical_class():
+    # Расписание
+    # Замены
+    # Календарь событий
+    # return render_template('technical_class.html')
+    pass
 
 
 @app.route('/news')
@@ -87,5 +105,72 @@ def languages(name):
     return render_template('languages.html', title=name.replace('_', ' ').title(), language=name.replace('_', ' ').title(), about=args_pages[name][0], image=args_pages[name][1])
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == current_user.id).first()
+    return render_template('profile.html', title='Профиль', name=user.name, about=user.about)
+
+
+@app.route('/profile/editing')
+@login_required
+def profile():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == current_user.id).first()
+    print(user)
+    return render_template('profile_editing.html', title='Редактирование профиля', name=user.name, about=user.about)
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/users.sqlite")
     app.run(port=8080, host='127.0.0.1')
